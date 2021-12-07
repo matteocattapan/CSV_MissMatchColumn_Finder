9d711706524e6d0bb1be3d1d8961537c608cefb2
matteocattapan <matteo.cattapan@gamsoft.it>
2021-12-07 10:32:14 +0100

    load csv data from file

---
+++ CSV_MissMatchColumn_Finder/CsvData.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CSV_MissMatchColumn_Finder
+{
+    public class CsvData
+    {
+        public string CSVFileName { get; set; }
+        public int Row { get; set; }
+        public string Value { get; set; }
+
+        public bool TryParse(string row_data, int row_count, int column, string csv_name)
+        {
+            try
+            {
+                this.CSVFileName = csv_name;
+                this.Row = row_count;
+                this.Value = row_data.Split(',')[column];
+                return true;
+
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+        }
+    }
+}
--- CSV_MissMatchColumn_Finder/Program.cs
+++ CSV_MissMatchColumn_Finder/Program.cs
@@ -1,9 +1,15 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace CSV_MissMatchColumn_Finder
 {
     class Program
     {
+
+        private static List<CsvData> _csvSourceData1 = new List<CsvData>();
+        private static List<CsvData> _csvSourceData2 = new List<CsvData>();
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello! We start matching soon..");
@@ -14,17 +20,57 @@ namespace CSV_MissMatchColumn_Finder
             // [2] _> csv path 2
             // [3] _> column match 2
 
-            var csv_path_1 = args[0];
-            int.TryParse(args[1], out var column_match_1);
-            var csv_path_2 = args[2];
-            int.TryParse(args[3], out var column_match_2);
+            var csvPath1 = args[0];
+            int.TryParse(args[1], out var columnMatch1);
+            var csvPath2 = args[2];
+            int.TryParse(args[3], out var columnMatch2);
 
             Console.WriteLine("### CONFIGURATION ###");
-            Console.WriteLine($"Path_1: {csv_path_1}");
-            Console.WriteLine($"Column_1: {column_match_1}");
-            Console.WriteLine($"Path_2: {csv_path_2}");
-            Console.WriteLine($"Column_2: {column_match_2}");
+            Console.WriteLine($"Path_1: {csvPath1}");
+            Console.WriteLine($"Column_1: {columnMatch1}");
+            Console.WriteLine($"Path_2: {csvPath2}");
+            Console.WriteLine($"Column_2: {columnMatch2}");
             Console.WriteLine("######");
+
+            LoadCsv(csvPath1, columnMatch1, ref _csvSourceData1);
+            LoadCsv(csvPath2, columnMatch2, ref _csvSourceData2);
+
+            Console.WriteLine($"Source 1: {_csvSourceData1.Count}");
+            Console.WriteLine($"Source 2: {_csvSourceData2.Count}");
+        }
+
+        private static void LoadCsv(string path, int column, ref List<CsvData> source)
+        {
+            using var reader = new StreamReader(File.OpenRead(path));
+            var currentRow = 1;
+
+            Console.WriteLine($"Start import.. {Path.GetFileName(path)}");
+            while (!reader.EndOfStream)
+            {
+                var line = reader.ReadLine();
+                if (string.IsNullOrEmpty(line))
+                {
+                    currentRow++;
+                    continue;
+                }
+
+                if (currentRow == 1)
+                {
+                    //Jump CSV Header
+                    currentRow++;
+                    continue;
+                }
+
+                var data = new CsvData();
+                if (data.TryParse(line, currentRow, column, Path.GetFileName(path)))
+                {
+                    source.Add(data);
+                }
+
+                currentRow++;
+            }
+
+            Console.WriteLine($"Import complete.");
         }
     }
 }
